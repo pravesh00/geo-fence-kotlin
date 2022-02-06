723dd84bee302fc2a77edac8648cbc1506e9f937
pravesh <pravesh_c@me.iitr.ac.in>
2022-02-07 03:32:59 +0530

    banner setup done

--- app/src/main/java/com/example/geo_fence_android/MainActivity.kt
+++ app/src/main/java/com/example/geo_fence_android/MainActivity.kt
@@ -5,8 +5,10 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.geo_fence_android.model.LocationInfo
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -30,6 +32,11 @@ lateinit var latitude:String
 lateinit var longitude:String
 private var mapFragment: SupportMapFragment? =null
 private var fab:View?=null
+private var zoomLevel:Int?=null
+private var marker:MarkerOptions?=null
+lateinit var oldLat:String
+lateinit var oldLng:String
+lateinit var banner:TextView
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +46,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         initUI()
 
         var sharedPreferences=getSharedPreferences("Geo-fence", MODE_PRIVATE)
+        var editor= sharedPreferences.edit()
 
         fab?.setOnClickListener {
             openSettingsActivity()
@@ -49,14 +57,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         longitude=sharedPreferences.getString("longitude","0").toString()
         username="default"
 
-        setupRoam()
-
-    }
-
-    private fun setupRoam() {
-        var sharedPreferences=getSharedPreferences("Geo-fence", MODE_PRIVATE)
-        var editor= sharedPreferences.edit()
-
         Roam.initialize(this, "f47884af842c8548f9c3b654e758158bcef04a20c0c755444b31adb58249f23d")
 
         if (!Roam.checkLocationServices()) {
@@ -66,7 +66,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && !Roam.checkBackgroundLocationPermission()) {
             Roam.requestBackgroundLocationPermission(this)
         }
-        if(sharedPreferences.getString("userID","").toString()===""){
+        if(sharedPreferences.getString("userID","").toString()==""){
             Roam.createUser("dfd",null,object : RoamCallback {
                 override fun onSuccess(roamUser: RoamUser) {
                     editor.putString("userID",roamUser.userId)
@@ -76,9 +76,40 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 override fun onFailure(roamError: RoamError) {
                     Log.e("user",roamError.message)
                 }
-            })}
+            })
+        }
+        else{
+            var userId=sharedPreferences.getString("userId","default");
+            Roam.getUser(userId, object : RoamCallback {
+                override fun onSuccess(roamUser: RoamUser) {
+                }
 
+                override fun onFailure(roamError: RoamError) {
+                }
+            })
+        }
         if(sharedPreferences.getString("latitude",null)==null) {
+            fetchLocationAndUpdate()
+        }else{
+            oldLat=sharedPreferences.getString("latitude",null).toString()
+            oldLng=sharedPreferences.getString("longitude",null).toString()
+            updateMap()
+        }
+
+    }
+
+
+    private fun fetchLocationAndUpdate() {
+        if (!Roam.checkLocationServices()) {
+            Roam.requestLocationServices(this)
+        } else if (!Roam.checkLocationPermission()) {
+            Roam.requestLocationPermission(this)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && !Roam.checkBackgroundLocationPermission()) {
+            Roam.requestBackgroundLocationPermission(this)
+        }
+        var sharedPreferences=getSharedPreferences("Geo-fence", MODE_PRIVATE)
+        var editor= sharedPreferences.edit()
+
             Roam.getCurrentLocation(
                 RoamTrackingMode.DesiredAccuracy.HIGH,
                 10,
@@ -88,6 +119,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                         Log.d("loc", p0?.latitude.toString())
                         latitude = p0?.latitude.toString()
                         longitude = p0?.longitude.toString()
+                        oldLat= latitude
+                        oldLng= longitude
                         editor.putString("latitude", latitude)
                         editor.putString("longitude", longitude)
                         editor.apply()
@@ -98,40 +131,101 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                         Log.d("loc", roamError.message)
                     }
                 })
-        }else{
-            updateMap()
-        }
+
+
     }
 
     private fun initUI() {
         mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
-
+        banner=findViewById(R.id.txtBanner)
         fab =findViewById(R.id.fab);
 
+    }
+    fun getZoomLevel(circle: Circle?){
+        if (circle != null) {
+            val radius = circle.radius
+            val scale = radius / 500
+            zoomLevel = (15 - Math.log(scale) / Math.log(2.0)).toInt()
+        }
+    }
+
+    private fun distance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
+        val theta = lon1 - lon2
+        var dist = (Math.sin(deg2rad(lat1))
+                * Math.sin(deg2rad(lat2))
+                + (Math.cos(deg2rad(lat1))
+                * Math.cos(deg2rad(lat2))
+                * Math.cos(deg2rad(theta))))
+        dist = Math.acos(dist)
+        dist = rad2deg(dist)
+        dist = dist * 60 * 1.1515
+        return dist
+    }
+
+    private fun deg2rad(deg: Double): Double {
+        return deg * Math.PI / 180.0
+    }
+
+    private fun rad2deg(rad: Double): Double {
+        return rad * 180.0 / Math.PI
     }
 
     private fun updateMap(){
         mapFragment?.getMapAsync(object : OnMapReadyCallback {
             override fun onMapReady(googleMap: GoogleMap) {
                 val loc = LatLng(latitude.toDouble(), longitude.toDouble())
-                googleMap.addMarker(
-                    MarkerOptions()
-                        .position(loc)
-                        .title("Your Location")
+                marker=MarkerOptions().position(loc).title("Your Currrent Location")
+
+                var markerMap= googleMap.addMarker(
+                    marker!!
                 )
+
                 val circle: Circle = googleMap.addCircle(
                     CircleOptions()
-                        .center(LatLng(latitude.toDouble(), longitude.toDouble()))
+                        .center(LatLng(oldLat.toDouble(), oldLng.toDouble()))
                         .radius(radius.toDouble())
                         .fillColor(Color.parseColor("#7f0A95FF"))
                         .strokeWidth(0f)
                 )
+                val handler = Handler()
+                handler.postDelayed(object : Runnable {
+                    override fun run() {
+
+                        Roam.getCurrentLocation(
+                            RoamTrackingMode.DesiredAccuracy.HIGH,
+                            10,
+                            object : RoamLocationCallback {
+
+                                override fun location(p0: android.location.Location?) {
+                                    Log.d("loc", p0?.latitude.toString())
+                                    marker=MarkerOptions().position(LatLng(p0?.latitude!!,p0.longitude)).title("Your current Location")
+                                    markerMap?.remove()
+                                    var newMarker=googleMap.addMarker(marker!!)
+                                    markerMap=newMarker
+                                    if(distance(oldLat.toDouble(),p0.latitude, oldLng.toDouble(),p0.longitude)> radius.toDouble()){
+                                        banner.setText("Out of Range")
+                                    }else{
+                                        banner.setText("You are in Range")
+                                    }
+
+                                }
+
+                                override fun onFailure(roamError: RoamError) {
+                                    Log.d("loc", roamError.message)
+                                }
+                            })
+
+                        handler.postDelayed(this, 1000)//1 sec delay
+                    }
+                }, 0)
+
+                getZoomLevel(circle)
                 googleMap.animateCamera(
                     CameraUpdateFactory.newLatLngZoom(
                         loc,
-                        15f
+                        zoomLevel?.toFloat()!!
                     )
                 )
             }
--- app/src/main/java/com/example/geo_fence_android/SettingsActivity.kt
+++ app/src/main/java/com/example/geo_fence_android/SettingsActivity.kt
@@ -49,13 +49,12 @@ class SettingsActivity : AppCompatActivity() {
         txtRadius.setText(data.getRadius())
         btnCancel.setOnClickListener { view->cancel() }
         btnSubmit.setOnClickListener { view->submit() }
+
         txtRadius.addTextChangedListener(object: TextWatcher {
             override fun afterTextChanged(s: Editable?) {
             }
-
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
-
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 data.setRadius(s.toString())
             }
